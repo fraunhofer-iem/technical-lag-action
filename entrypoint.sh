3a953d32029c82857459cba673163da1a509abc3
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-07-20 13:34:39 +0200

    fixed paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,12 @@ outputPath=/github/workspace/$2
 # we move the package manager files to a separate directory to make it easier for the ORT
 
 # Base directory where package files will be copied
-DESTINATION_DIR="tmp/packageFiles"
-
+DESTINATION_DIR="/tmp/packageFiles"
+GRAPH_DIR="/tmp/graphs"
 # Create the base destination directory if it doesn't exist
 mkdir -p "$DESTINATION_DIR"
 
+mkdir -p "$GRAPH_DIR"
 # Function to copy package files if they exist
 copy_package_files() {
     local src_dir=$1
@@ -31,5 +32,5 @@ copy_package_files() {
 
 copy_package_files "$inputPath" "$DESTINATION_DIR"
 
-sh /app/build/install/technical-lag-calculator/bin/technical-lag-calculator create-dependency-graph --projects-dir "$DESTINATION_DIR" --output-path /graphs 
-sh /app/build/install/technical-lag-calculator/bin/technical-lag-calculator calculate-technical-lag --dependency-graph-dirs /graphs --output-path $outputPath 
+sh /app/build/install/technical-lag-calculator/bin/technical-lag-calculator create-dependency-graph --projects-dir /tmp --output-path "$GRAPH_DIR" 
+sh /app/build/install/technical-lag-calculator/bin/technical-lag-calculator calculate-technical-lag --dependency-graph-dirs "$GRAPH_DIR" --output-path $outputPath 
