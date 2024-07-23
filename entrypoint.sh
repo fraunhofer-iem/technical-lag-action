8241f0f598d9f84490d5aee4590796f34e0d14ba
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-07-23 13:53:58 +0200

    fixed duplicate copying in nested folders

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,28 @@
 #!/bin/sh
 
 set -e
-# we probably need to map this with /github/workspace
+
 inputPath=/github/workspace/$1
 outputPath=/github/workspace/$2
-# we move the package manager files to a separate directory to make it easier for the ORT
 analyzeSubDir=$3
-# Base directory where package files will be copied
+
 DESTINATION_DIR="/tmp/packageFiles"
 GRAPH_DIR="/tmp/graphs"
-# Create the base destination directory if it doesn't exist
-mkdir -p "$DESTINATION_DIR"
 
+mkdir -p "$DESTINATION_DIR"
 mkdir -p "$GRAPH_DIR"
+
 # Function to copy package files if they exist
 copy_package_files() {
     local src_dir=$1
     local dest_dir=$2
-    echo "Atempting to copy files from $src_dir to $dest_dir"
     mkdir -p "$dest_dir"
-    # Copy the package files if they exist
-    if [ -f "$src_dir/package.json" ]; then
-        cp "$src_dir/package.json" "$dest_dir"
-    fi
-    if [ -f "$src_dir/package-lock.json" ]; then
-        cp "$src_dir/package-lock.json" "$dest_dir"
-    fi
-    if [ -f "$src_dir/yarn.lock" ]; then
-        cp "$src_dir/yarn.lock" "$dest_dir"
-    fi
+    for file in package.json package-lock.json yarn.lock; do
+        if [ -f "$src_dir/$file" ]; then
+            cp "$src_dir/$file" "$dest_dir"
+            echo "Copied $file from $src_dir to $dest_dir"
+        fi
+    done
 }
 
 # Function to copy package files from inputPath and optionally from subdirectories
@@ -37,12 +31,12 @@ copy_files() {
     local dest_dir=$2
 
     # Copy package files from the base directory
-    copy_package_files "$base_dir" "$dest_dir"/root
+    copy_package_files "$base_dir" "$dest_dir/root"
 
     # If analyzeSubDir is true, iterate over subdirectories and copy package files
     if [ "$analyzeSubDir" = "true" ]; then
         echo "Started analyzing subdirectories"
-        find "$base_dir" -type d | while read -r dir; do
+        find "$base_dir" -mindepth 1 -type d | while read -r dir; do
             # Create corresponding directory structure in DESTINATION_DIR
             relative_path="${dir#$base_dir/}"
             echo "Sub dir relative path $relative_path"
@@ -55,4 +49,4 @@ copy_files() {
 copy_files "$inputPath" "$DESTINATION_DIR"
 
 sh /app/build/install/technical-lag-calculator/bin/technical-lag-calculator create-dependency-graph --projects-dir "$DESTINATION_DIR" --output-path "$GRAPH_DIR" 
-sh /app/build/install/technical-lag-calculator/bin/technical-lag-calculator calculate-technical-lag --dependency-graph-dirs "$GRAPH_DIR" --output-path $outputPath 
+sh /app/build/install/technical-lag-calculator/bin/technical-lag-calculator calculate-technical-lag --dependency-graph-dirs "$GRAPH_DIR" --output-path "$outputPath"
